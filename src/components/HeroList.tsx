ce181585d0151cb46536a6f6d2d5879873a54226
Cory Hoban <hobancor@gmail.com>
2024-03-15 15:12:02 -0400

    add match ups

--- src/components/HeroList.tsx
+++ src/components/HeroList.tsx
@@ -1,48 +1,88 @@
-import { useHeroes } from "../hooks/hooks";
-import { Box, Button, Grid, ImageList, ImageListItem, Stack, Typography } from "@mui/material";
+import { useHeroes, useHeroMatchups } from "../hooks/hooks";
+import { Box, Button, Grid, ImageList, ImageListItem, Stack, Typography, ToggleButtonGroup, toggleButtonGroupClasses, ToggleButton } from "@mui/material";
+import { styled } from '@mui/material/styles';
 import { useMemo, useState } from "react";
+import { motion } from "framer-motion";
+
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+  [`& .${toggleButtonGroupClasses.grouped}`]: {
+    margin: theme.spacing(0.5),
+    color: 'red',
+    background: 'white',
+    border: 0,
+    borderRadius: theme.shape.borderRadius,
+    [`&.${toggleButtonGroupClasses.disabled}`]: {
+      border: 0,
+    },
+  },
+  [`& .${toggleButtonGroupClasses.middleButton},& .${toggleButtonGroupClasses.lastButton}`]:
+    {
+      marginLeft: -1,
+      borderLeft: '1px solid transparent',
+    },
+}));
 
 const HeroList = () => {
-  const [heroType, setHeroType] = useState<Array<string>>(['agi', 'str', 'int', 'all']);
+  const [attributeFilter, setAttributeFilter] = useState(() => ['agi', 'str', 'int', 'all']);
+  const [selectedHero, setSelectedHero] = useState<number>(0);
   const {data} = useHeroes();
+  const {data: heroMatchups, refetch} = useHeroMatchups(selectedHero);
+
+  console.log(heroMatchups);
 
   type HeroStats = {
+    id: number
     localized_name: string,
     primary_attr: string,
     img: any,
     icon: any,
   }
 
-  const heroTypes = ['agi', 'str', 'int', 'all'];
 
-  const handleType = (type: string) => {
-    let newTypes = heroType;
-    if (heroType.includes(type)) {
-      newTypes = newTypes.filter((heroType: string) => heroType !== type);
-    } else {
-      newTypes.push(type);
-    }
-    setHeroType(newTypes);
-  }
+  const heroList = useMemo(() => data.filter((hero: HeroStats) => (attributeFilter.includes(hero.primary_attr))),
+    [attributeFilter]);
 
-  const heroList = useMemo(() => data.filter((hero: HeroStats) => (hero.primary_attr)),
-    [heroType]);
+  const handleAttributeFilter = (
+    event: React.MouseEvent<HTMLElement>,
+    newFormats: string[],
+  ) => {
+    setAttributeFilter(newFormats);
+  };
 
   return (
     <Box>
       <Stack flexDirection="row">
-        <Button variant='contained' onClick={() => handleType("agi")}>Agility</Button>
-        <Button onClick={() => handleType("str")}>Strength</Button>
-        <Button onClick={() => handleType("int")}>Intelligence</Button>
-        <Button onClick={() => handleType("all")}>Universal</Button>
+        <ToggleButtonGroup
+          color={'primary'}
+          value={attributeFilter}
+          onChange={handleAttributeFilter}
+          aria-label="attribute filter"
+        >
+          <ToggleButton
+            value={'agi'}
+            aria-label="agility"
+            sx={{
+              background: 'white',
+              color: 'red',
+            }}
+          >
+            Agility
+          </ToggleButton>
+          <ToggleButton value={'str'} aria-label="strength">Strength</ToggleButton>
+          <ToggleButton value={'int'} aria-label="intelligence">Intelligence</ToggleButton>
+          <ToggleButton value={'all'} aria-label="universal">Universal</ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
       <Grid container>
         <Grid item xs={12}>
           <ImageList cols={4} rowHeight={130}>
             {heroList.map((hero: HeroStats) => {
-              return <ImageListItem sx={{margin: 1}}>
-                <img src={`https://cdn.cloudflare.steamstatic.com/${hero.img}`} srcSet={`https://cdn.cloudflare.steamstatic.com/${hero.img}`}/>
-              </ImageListItem>
+              return (
+                <ImageListItem sx={{margin: 1}} onClick={() => {setSelectedHero(hero.id); refetch()}}>
+                  <motion.div animate={{scale:1}} initial={{scale:0}}>
+                    <img src={`https://cdn.cloudflare.steamstatic.com/${hero.img}`} srcSet={`https://cdn.cloudflare.steamstatic.com/${hero.img}`}/>
+                  </motion.div>
+                </ImageListItem>)
             })}
           </ImageList>
         </Grid>
--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,8 +1,12 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { client } from "../api";
 
 const getHeroStats = () => {
   return client.get('/heroStats');
+}
+
+const getHeroMatchup = (heroId: number) => {
+  return client.get(`/heroes/${heroId}/matchups`);
 }
 
 export const useHeroes = () => {
@@ -10,5 +14,14 @@ export const useHeroes = () => {
     queryKey: ['heroes'],
     queryFn: getHeroStats,
     select: data => data.data,
+  })
+}
+
+export const useHeroMatchups = (heroId: number) => {
+  return useQuery({
+    queryKey: ['matchup'],
+    queryFn: () => getHeroMatchup(heroId),
+    select: data => data.data,
+    enabled: false
   })
 }
